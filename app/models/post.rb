9fe41ab20d3cec213cb10fc2fcff166c9c052cf4
Yusheng <tamamali@icloud.com>
2015-07-12 15:57:08 +0800

    add before_save callback for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,13 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, format: { with: /\A[0-9]/, message: 'Should start with number.' }  #/\A\d/
-  before_create :generate_content_from_title
+  before_create  :generate_content_from_title
+  before_save    :upcase_title
 
   protected
     def generate_content_from_title
       self.content = title if content.blank?
     end 
+    def upcase_title
+      self.title.upcase!
+    end
 end
