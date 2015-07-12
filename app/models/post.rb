806aefa9c70c91abc9fe4f2b6a74e1e3a42c7e7a
Yusheng <tamamali@icloud.com>
2015-07-12 15:51:04 +0800

    add callback action for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, format: { with: /\A[0-9]/, message: 'Should start with number.' }  #/\A\d/
+  before_create :generate_content_from_title
+
+  protected
+    def generate_content_from_title
+      self.content = title if content.blank?
+    end 
 end
