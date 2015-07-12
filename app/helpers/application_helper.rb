b6084c54893148e31cdd552bd128af88856b668d
Yusheng <tamamali@icloud.com>
2015-07-12 10:39:54 +0800

    try to pass Hash into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def nav_li name, url
-    content_tag :li, link_to(name, url)
+  def nav_li name, url, options={}
+    content_tag :li, link_to(name, url, options)
   end
 end
