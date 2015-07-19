eb68da22ac31c262e88ad4a2c5607ed1943f93b1
Yusheng <tamamali@icloud.com>
2015-07-19 10:32:55 +0800

    defines a create action for PostsController and create a view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,18 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.new({
+      title: params[:title]
+      content: params[:content]
+    })
+    if @post.save
+      redirect_to post_path(@post.id)
+      # redirect_to post This can work like above also, you can pass an ActiveRecord to link to that path
+    else
+      # if validates error, back to the form.
+      render :new
+    end
+  end
 end
