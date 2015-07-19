431caf3c9905e9c89b0cfa6ef091bd17efe85eaf
Yusheng <tamamali@icloud.com>
2015-07-19 11:03:33 +0800

    use a better way to submit form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,13 +7,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new({
-      title: params[:title],
-      content: params[:content]
-    })
+    @post = Post.new params[:post].to_hash
+    # @post = Post.new({
+    #   title: params[:title],
+    #   content: params[:content]
+    # })
     if @post.save
       redirect_to post_path(@post.id)
-      # redirect_to post This can work like above also, you can pass an ActiveRecord to link to that path
+      # redirect_to @post # This can work like above also, you can pass an ActiveRecord to link to that path
     else
       # if validates error, back to the form.
       render :new
