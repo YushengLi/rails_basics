a968b1f6cbdaa1d28f7219e3d8d7854f7817c3fa
Yusheng <tamamali@icloud.com>
2015-07-19 12:15:46 +0800

    remove duplicate code and move it to new functions in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update]
+
   def index
     @post = Post.all
   end
+  
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -11,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new params.require(:post).permit(:title, :content)
+    @post = Post.new post_params
     # @post = Post.new({
     #   title: params[:title],
     #   content: params[:content]
@@ -26,15 +28,22 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update params.require(:post).permit(:title, :content)
+    if @post.update post_params
       redirect_to @post
     else
       render :edit
     end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
